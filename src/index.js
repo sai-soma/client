d565dae0d437dc7ead8b97b1c0e193ba9011849a
app1 <saieesh.soma1919@gmail.com>
2025-07-04 11:35:06 +0530

    Debug blank screen: HashRouter

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import { HashRouter } from 'react-router-dom';
 import "@fontsource/poppins";
 import { ContextProvider } from "./context/StateContext";
 
@@ -9,9 +10,9 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <ContextProvider>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </ContextProvider>
   </React.StrictMode>
 );
